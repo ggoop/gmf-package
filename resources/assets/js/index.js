e307e8b39b5048bb493a414fc347a8fd518cabd8
pull user <pull user@163.com>
2018-05-01 20:28:53 +0800

    init

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,5 +1,26 @@
+
+import config from 'gmf/config';
+import routesWeb from './routes/web';
 import components from './components';
+
 const options = {
   components
+};
+options.install = (Vue) => {
+  if (options.installed) {
+    console.warn('Vue components is already installed.');
+    return;
+  }
+  for (let component in options) {
+    const componentInstaller = options[component];
+
+    if (componentInstaller && component !== 'install') {
+      Vue.use(componentInstaller);
+    }
+  }
+  //配置应用包路由
+  config.route(routesWeb);
+
+  options.installed = true;
 };
 export default options;
